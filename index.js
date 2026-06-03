ba1946d823744443cfb8fb5cac4fc0fb72491a2c
Vishesh Krishna S A <visheshkrishnasa@gmail.com>
2026-06-03 13:34:39 +0530

    Add Event Listner to Movie selection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const moviesList = [
-	{ movieName: "Devil May Cry", price: 150 },
-	{ movieName: "Kill Blue", price: 100 },
-	{ movieName: "Attack on Titan", price: 250 },
+	{ movieName: "Devil May Cry", price: 15 },
+	{ movieName: "Kill Blue", price: 10 },
+	{ movieName: "Attack on Titan", price: 25 },
 ];
 
 // * adding movies to the dropdown list
@@ -13,15 +13,28 @@ const movieDate = document.querySelector(".date");
 const movieDefault = document.createElement("option");
 movieDefault.value = "";
 movieDefault.textContent = "--Please choose an option--";
-movieName.textContent = "Please select a movie";
+movieName.textContent = "N/A";
 moviePrice.textContent = "$ 0";
-movieDate.textContent = "N/A";
+const today = new Date();
+movieDate.textContent = today.toLocaleDateString("en-US", {
+	day: "2-digit",
+	month: "long",
+	year: "numeric",
+});
+
 movies.append(movieDefault);
 // movies 1 to 3
 moviesList.forEach((movie) => {
 	const option = document.createElement("option");
 	option.value = movie.movieName;
 	option.textContent = movie.movieName;
-	// todo: add event listener to the dropdown list
+	// event listener for each movie
+	movies.addEventListener("change", (e) => {
+		if (e.target.value === movie.movieName) {
+			movieName.textContent = movie.movieName;
+			moviePrice.textContent = `$ ${movie.price}`;
+		}
+		
+	});
 	movies.appendChild(option);
 });
